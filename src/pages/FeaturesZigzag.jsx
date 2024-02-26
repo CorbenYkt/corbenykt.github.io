f5cedc507710c8506e1dc2a1f48816763eef43b8
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-02-26 17:32:43 +1300

    links fixed

--- src/pages/FeaturesZigzag.jsx
+++ src/pages/FeaturesZigzag.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
 import Project1 from '../img/features-03-image-01.png';
 import Project2 from '../img/features-03-image-02.png';
 import Project3 from '../img/features-03-image-03.png';
@@ -18,7 +16,7 @@ function FeaturesZigzag() {
 
           {/* ProjectsSection */}
           <div id="projectssection" className="grid gap-20">
-            <Link to='https://corbenykt.github.io/fozzy' className='relative flex flex-col items-center'>
+            <a href='https://corbenykt.github.io/fozzy' className='relative flex flex-col items-center'>
               {/* Fozzy's page */}
               <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                 {/* Image */}
@@ -27,18 +25,16 @@ function FeaturesZigzag() {
                 </div>
                 {/* Content */}
                 <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
-
                   <div className="md:pr-4 lg:pr-12 xl:pr-16">
                     <h3 className="h3 mb-3 text-black">Fozzy's page</h3>
                     <p className="text-xl text-gray-500 mb-4">Yes, this site is about our lovely dog Fozzy. Fozzy is a West Highland White Terrier, he is already 11 years old and a great family friend</p>
                   </div>
-
                 </div>
               </div>
-            </Link>
+            </a>
 
             {/* AndreyChickachev's page */}
-            <Link to='https://www.andreychikachev.ru' className='relative flex flex-col items-center'>
+            <a href='https://www.andreychikachev.ru' className='relative flex flex-col items-center'>
               <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                 {/* Image */}
                 <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
@@ -55,10 +51,10 @@ function FeaturesZigzag() {
                   </div>
                 </div>
               </div>
-            </Link>
+            </a>
 
             {/* 3rd item */}
-            <Link to='https://corbenykt.github.io/currencycalculator' className='relative flex flex-col items-center'>
+            <a href='https://corbenykt.github.io/currencycalculator' className='relative flex flex-col items-center'>
               <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                 {/* Image */}
                 <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
@@ -71,10 +67,9 @@ function FeaturesZigzag() {
                     <p className="text-xl text-gray-500 mb-4">And this is my simple currency calculator. While moving, I needed a quick conversion tool that was fast and convenient. This is how the idea came up to make a small calculator for myself. Currency conversion takes place according to current data from the Central Bank</p>
                   </div>
                 </div>
-              </div></Link>
-
+              </div>
+            </a>
           </div>
-
         </div>
       </div>
     </section>
--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,51 +6,36 @@ function Footer() {
     <footer>
       <div className="py-12 md:py-16">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-          {/* Top area: Blocks */}
-          <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12">
-
-            {/* 1st block */}
-            <div className="md:col-span-4 lg:col-span-5">
-              <div className="mb-2">
-              </div>
+          <div className="columns-3">
+            <div className="text-sm">
+              <h6 className="text-gray-500 font-medium mb-1">E-mail:</h6>
+              <ul>
+                <li className="mb-1">
+                  <a href="mailto:vool34@gmail.com" className="text-gray-500 hover:text-gray-200 transition duration-150 ease-in-out">vool34@gmail.com</a>
+                </li>
+              </ul>
             </div>
 
-            {/* 2nd, 3rd and 4th blocks */}
-            <div className="md:col-span-8 lg:col-span-7 grid sm:grid-cols-3 gap-8">
-
-              {/* 2nd block */}
-              <div className="text-sm">
-                <h6 className="text-gray-500 font-medium mb-1">E-mail:</h6>
-                <ul>
-                  <li className="mb-1">
-                    <Link to="mailto:vool34@gmail.com" className="text-gray-500 hover:text-gray-200 transition duration-150 ease-in-out">vool34@gmail.com</Link>
-                  </li>
-                </ul>
-              </div>
-
-              {/* 3rd block */}
-              <div className="text-sm">
-                <h6 className="text-gray-500 font-medium mb-1">Phone:</h6>
-                <ul>
-                  <li className="mb-1 text-gray-500 hover:text-gray-200 transition duration-150 ease-in-out">
-                    +64 22 594 6268
-                  </li>
-                </ul>
-              </div>
-
-              {/* 4th block */}
-              <div className="text-sm">
-                <h6 className="text-gray-500 font-medium mb-1">GIT:</h6>
-                <ul>
-                  <li className="mb-1">
-                    <Link to="https://github.com/corbenykt" className="text-gray-500 hover:text-gray-100 transition duration-150 ease-in-out">https://github.com/corbenykt</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="text-gray-400 text-sm text-center">&copy; New Zealand, Wellington, Dmitrii Artemev</div>
+            <div className="text-sm">
+              <h6 className="text-gray-500 font-medium mb-1">Phone:</h6>
+              <ul>
+                <li className="mb-1 text-gray-500 hover:text-gray-200 transition duration-150 ease-in-out">
+                  +64 22 594 6268
+                </li>
+              </ul>
             </div>
+
+            <div className="text-sm">
+              <h6 className="text-gray-500 font-medium mb-1">GIT:</h6>
+              <ul>
+                <li className="mb-1">
+                  <a href="https://github.com/corbenykt" className="text-gray-500 hover:text-gray-100 transition duration-150 ease-in-out">https://github.com/corbenykt</a>
+                </li>
+              </ul>
+            </div>
+
           </div>
+          <div className="text-gray-400 text-sm text-center">&copy; New Zealand, Wellington, Dmitrii Artemev</div>
         </div>
       </div>
     </footer>
